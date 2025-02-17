d8350b18bf2e7b584a8b746c239a394bcd4deb18
Ali Houmani <houmani.datalu@gmail.com>
2025-02-17 02:19:52 +0200

    Modify File Structure
    Move the project implementation to /src

--- deploy/create_application.bat
+++ deploy/create_application.bat
@@ -1,7 +1,7 @@
 :: activate virtual environment
-cd "C:\Users\Admiin\anaconda3\condabin"
-call activate screen_time_env
-cd "C:\Ali\Learning\Software\On My Own\python projects\Desktop_Screen_Time_Tracker"
+:: replace the directory below by the actual directory of condabin\activate on your device
+call "C:\Users\Admiin\anaconda3\condabin\activate" screen_time_env
+cd "src"
 echo "Environment Activated"
 pause
 :: create executables
@@ -17,24 +17,28 @@ pause
 :: deactivate environment
 call conda deactivate
 echo "Environment Deactivated"
+cd ".."
 pause
 :: move executables to curr dir
-move ".\dist\*" "deploy/"
+move "src\dist\*" "deploy\"
 echo "Executables Moved"
+pause
 :: move chrome extension to curr dir
-xcopy /E /Y ".\extension" "deploy\extension\"
+xcopy /E /Y "src\extension" "deploy\extension\"
 echo "Extension Moved"
 pause
 :: remove unnecessary files
-rmdir /s /q "./dist"
-rmdir /s /q "./build"
+rmdir /s /q "src\dist"
+rmdir /s /q "src\build"
+pause
 :: remove unnecessary files
-del "./main.spec"
-del "./recorder.spec"
-del "./browser_recorder.spec"
+del "src\main.spec"
+del "src\recorder.spec"
+del "src\browser_recorder.spec"
 echo "Unnecessary Files Removed"
 pause
 :: create installer
-"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "deploy/setup.iss"
+:: replace the directory below by the actual directory of inno setup on your device
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "deploy\setup.iss"
 echo "Installer Created"
 pause
